fad808b37730b95504923737e7cc688548bc7e61
hkim4 <hkim4@DESKTOP-9ITNDN8>
2021-05-27 15:22:41 +0900

    2021.05.27 알고리즘 과제 중 해시함수가 필요하여 만듦

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {	
+	String input;
+	
+	String getKey() {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("input: ");
+		input = scan.next();
+		
+		return input;
+	}
+	
+	int hashFunction(String key) {
+		int output = 0;
+		
+		for(int i=0;i<input.length(); i++)
+			output += key.charAt(i);
+		
+		return output;
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		String key;
+		int output;
+		
+		key = main.getKey();
+		while(key != "-1") {
+			output = main.hashFunction(key);
+			System.out.println("output: " + output);
+			
+			key = main.getKey();
+		}
+	}
+}
